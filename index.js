b062a4fdaae87d74ee6836765b202becebff29de
Aman Singh <101695980+Amansingh0369@users.noreply.github.com>
2024-09-17 18:34:42 +0530

    todos backend is updated

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post("/todo",function (req,res){
+app.post("/todo", async function(req, res) {
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+    if (!parsePayload.success) {
+        res.status(411).json({
+
+            message: "you have sent wrong input"
+        })
+        return;
+    }
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+
+    res.json({
+        message: "Todo is created"
+    })
 
 })
-app.get("/todos",function (req,res){
+app.get("/todos", async function(req, res) {
+    const todos = awaittodo.find({});
+    res.json({
+        todos
+    });
 
 })
-app.put("/todo",function (req,res){
+app.put("/todo", async function(req, res) {
+    const updatePaylaod = req.body;
+    const parsePayload = updateTodo.safeParse(updatePaylaod);
+    if (!parsePayload.success) {
+        res.status(411).json({
 
+            message: "you have sent wrong input"
+        })
+        return;
+    }
+    await todo.update({
+        _id: req.body._id
+    }, {
+        completed: true
+    })
+    res.json({
+        message: "Todo is updated"
+    })
 })
 
 app.listen(port);
