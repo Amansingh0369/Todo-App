a1310c4f81fe6f67be28a57ef8c19cb923d8200f
Aman Singh <101695980+Amansingh0369@users.noreply.github.com>
2024-09-17 17:36:41 +0530

    created index.js and types.js for zod

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post("/todo",function (req,res){
+
+})
+app.get("/todos",function (req,res){
+
+})
+app.put("/todo",function (req,res){
+
+})
+
+app.listen(port);
