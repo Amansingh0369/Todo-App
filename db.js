fb0bc614242bf82403e6d3c40bbdcb3e2b9e0b7f
Aman Singh <101695980+Amansingh0369@users.noreply.github.com>
2024-09-17 18:41:56 +0530

    typo error in db.js

--- db.js
+++ db.js
@@ -3,12 +3,12 @@ const { boolean } = require("zod");
 mongoose.connect("mongodb+srv://singh0369aman:Amansingh0369@aman.6sexufp.mongodb.net/Todo-App")
 
 const todoSchema = mongoose.Schema({
-    title: string,
-    description: string,
-    completed: boolean
+    title: String,
+    description: String,
+    completed: Boolean
 })
 
 const todo = mongoose.model("todos", todoSchema);
-module.export = {
+module.exports = {
     todo
 }
